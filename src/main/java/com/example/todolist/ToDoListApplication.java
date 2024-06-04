e5fd5ed947fb98806ef6bb824b34dff5b87844a0
OisinWrites <oisin.banville@gmail.com>
2024-06-04 10:49:21 +0100

    Share jawsdb database instance across local development and production profiles

--- src/main/java/com/example/todolist/ToDoListApplication.java
+++ src/main/java/com/example/todolist/ToDoListApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ToDoListApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(ToDoListApplication.class, args);
     }
 
